42dbf901dce8a20daccea9706ad9558a6969387c
NicoGarcia12 <nicolasgarcia9812@hotmail.com>
2024-03-10 16:53:01 -0300

    NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import styles from "./NavBar.module.css";
+
 export default function NavBar() {
+  const usuarioCookie = Cookies.get("usuario");
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Cookies.remove("usuario");
+    navigate("/login");
+  };
 
   return (
-    <div>
-      <h1>NAVBAR</h1>
+    <div className={styles["navbar-container"]}>
+      <NavLink to="/systems" className={styles.navLink}>
+        <button className={styles["systems-button"]}>Home</button>
+      </NavLink>
+      <span className={styles.username}>Bienvenido {usuarioCookie}</span>
+      <button className={styles["logout-button"]} onClick={handleLogout}>
+        Cerrar sesión
+      </button>
     </div>
   );
 }
--- src/components/ayuda.js
+++
@@ -1,7 +0,0 @@
-// Login
-
-// Hacer peticion a la API de todo y lo guardo en estado global
-
-// Selecciono 2 activos y me tiene que decir su comparativa
-
-// Calculadora debo permitir cambiar los valores y con un onChange del input vuelvo a pedir a l
